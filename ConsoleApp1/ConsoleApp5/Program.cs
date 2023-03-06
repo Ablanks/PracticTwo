05d7ee1ecace585758308b363153574f51c3d534
Ablanks <117064808+Ablanks@users.noreply.github.com>
2023-03-06 17:06:28 +0700

    1

---
+++ ConsoleApp1/ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызовем метод для получения данных о водителях
+            DatabaseRequests.GetTypeCarQuery();
+            Console.WriteLine();
+            // Добавим нового водителя в БД
+            DatabaseRequests.AddDriverQuery("Денис", "Иванов", DateTime.Parse("1997.01.12"));
+            // Вызовем метод для получения данных о водителях
+            DatabaseRequests.GetDriverQuery();
+        
+            // Вызовем метод для получения данных о типах автомобилей
+            DatabaseRequests.GetTypeCarQuery();
+            Console.WriteLine();
+            // Добавим новый тип автомобиля в БД
+            DatabaseRequests.AddTypeCarQuery("Воздушный");
+            // Вызовем метод для получения данных о типах автомобилей
+            DatabaseRequests.GetTypeCarQuery();
+            //
+        }
+    }
+}
